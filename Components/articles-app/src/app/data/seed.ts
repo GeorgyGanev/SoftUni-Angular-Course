b3ea84cd3b116110be0d7d4222bf912324f85a06
Georgy Ganev <106162345+GeorgyGanev@users.noreply.github.com>
2023-06-25 12:23:09 +0300

    complete homework
    Articles Site homework completed

---
+++ Components/articles-app/src/app/data/seed.ts
@@ -0,0 +1,44 @@
+const data = [
+  {
+    title: 'Article 1',
+    description:
+      'While the polar bear is mostly carnivorous, and the giant panda feeds almost entirely on bamboo, the remaining six species are omnivorous with varied diets. With the exception of courting individuals and mothers with their young, bears are typically solitary animals. They may be diurnal or nocturnal and have an excellent sense of smell. Despite their heavy build and awkward gait, they are adept runners, climbers, and swimmers. Bears use shelters, such as caves and logs, as their dens; most species occupy their dens during the winter for a long period of hibernation, up to 100 days.',
+    author: 'Author 1',
+    imageUrl:
+      'https://plus.unsplash.com/premium_photo-1675714692711-d1aac0262feb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmVhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    title: 'Article 2',
+    description:
+      'Bears have been hunted since prehistoric times for their meat and fur; they have been used for bear-baiting and other forms of entertainment, such as being made to dance. With their powerful physical presence, they play a prominent role in the arts, mythology, and other cultural aspects of various human societies. In modern times, bears have come under pressure through encroachment on their habitats and illegal trade in bear parts, including the Asian bile bear market. The IUCN lists six bear species as vulnerable or endangered, and even least concern species, such as the brown bear, are at risk of extirpation in certain countries. The poaching and international trade of these most threatened populations are prohibited, but still ongoing.',
+    author: 'Author 2',
+    imageUrl:
+      'https://images.unsplash.com/photo-1568162603664-fcd658421851?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YmVhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60',
+  },
+
+  {
+    title: 'Article 3',
+    description:'The English word "bear" comes from Old English bera and belongs to a family of names for the bear in Germanic languages, such as Swedish björn, also used as a first name. This form is conventionally said to be related to a Proto-Indo-European word for "brown", so that "bear" would mean "the brown one".[1][2] However, Ringe notes that while this etymology is semantically plausible, a word meaning "brown" of this form cannot be found in Proto-Indo-European. He suggests instead that "bear" is from the Proto-Indo-European word *ǵʰwḗr- ~ *ǵʰwér "wild animal".[3] This terminology for the animal originated as a taboo avoidance term: proto-Germanic tribes replaced their original word for bear—arkto—with this euphemistic expression out of fear that speaking the animals true name might cause it to appear. According to author Ralph Keyes, this is the oldest known euphemism.',
+    author: 'Author 3',
+    imageUrl:
+      'https://images.unsplash.com/photo-1551792714-9a8b35338793?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGJlYXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60',
+  },
+  {
+    title: 'Article 4',
+    description:
+      'The earliest members of Ursidae belong to the extinct subfamily Amphicynodontinae, including Parictis (late Eocene to early middle Miocene, 38–18 Mya) and the slightly younger Allocyon (early Oligocene, 34–30 Mya), both from North America. These animals looked very different from todays bears, being small and raccoon-like in overall appearance, with diets perhaps more similar to that of a badger. Parictis does not appear in Eurasia and Africa until the Miocene.[14] It is unclear whether late-Eocene ursids were also present in Eurasia, although faunal exchange across the Bering land bridge may have been possible during a major sea level low stand as early as the late Eocene (about 37 Mya) and continuing into the early Oligocene.[15] European genera morphologically very similar to Allocyon, and to the much younger American Kolponomos (about 18 Mya),[16] are known from the Oligocene, including Amphicticeps and Amphicynodon.[15] There has been various morphological evidence linking amphicynodontines with pinnipeds, as both groups were semi-aquatic, otter-like mammals.In addition to the support of the pinniped–amphicynodontine clade, other morphological and some molecular evidence supports bears being the closest living relatives to pinnipeds',
+    author: 'Author 4',
+    imageUrl: 'https://images.unsplash.com/photo-1573920111312-04f1b25c6b85?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGJlYXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60',
+  },
+
+  {
+    description:
+      'The subfamily Ursinae experienced a dramatic proliferation of taxa about 5.3–4.5 Mya, coincident with major environmental changes; the first members of the genus Ursus appeared around this time. The sloth bear is a modern survivor of one of the earliest lineages to diverge during this radiation event (5.3 Mya); it took on its peculiar morphology, related to its diet of termites and ants, no later than by the early Pleistocene. By 3–4 Mya, the species Ursus minimus appears in the fossil record of Europe; apart from its size, it was nearly identical to todays Asian black bear. It is likely ancestral to all bears within Ursinae, perhaps aside from the sloth bear. Two lineages evolved from U. minimus: the black bears (including the sun bear, the Asian black bear, and the American black bear); and the brown bears (which includes the polar bear). Modern brown bears evolved from U. minimus via Ursus etruscus, which itself is ancestral to the extinct Pleistocene cave bear.[26] Species of Ursinae have migrated repeatedly into North America from Eurasia as early as 4 Mya during the early Pliocene.[29][30] The polar bear is the most recently evolved species and descended from a population of brown bears that became isolated in northern latitudes by glaciation 400,000 years ago.',
+    title: 'Article 5',
+    author: 'Author 5',
+    imageUrl:
+      'https://media.istockphoto.com/id/1048741184/photo/brown-bear-in-winter.webp?b=1&s=170667a&w=0&k=20&c=rcZlJZ8HFpcCJmrufYuSEkk78YpyvsbYNifyNAK0Mqc=',
+  },
+];
+
+export { data };
